a2e11566d3e320132af70af72136c7693799bb7b
Shardul Gore <shardulgore@Sharduls-MacBook-Air.local>
2025-06-12 16:43:09 +0530

    crewai stockpicker (needs openai api)

---
+++ CrewAI/stock_picker/src/stock_picker/main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from datetime import datetime
+
+from stock_picker.crew import StockPicker
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+import os
+
+# Check if API keys are set
+required_keys = ["GEMINI_API_KEY", "GROQ_API_KEY", "SERPER_API_KEY"]
+for key in required_keys:
+    if os.getenv(key):
+        print(f"✅ {key} is set")
+    else:
+        print(f"❌ {key} is not set")
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+
+def run():
+    """
+    Run the research crew.
+    """
+    inputs = {
+        'sector': 'Technology',
+        # "current_date": str(datetime.now())
+    }
+
+    # Create and run the crew
+    result = StockPicker().crew().kickoff(inputs=inputs)
+
+    # Print the result
+    print("\n\n=== FINAL DECISION ===\n\n")
+    print(result.raw)
+
+
+if __name__ == "__main__":
+    run()
