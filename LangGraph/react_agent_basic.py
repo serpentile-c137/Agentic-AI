f834a42afc7ab8bc73fc237faa73211ad6b959a7
Shardul Gore <shardulgore@Sharduls-MacBook-Air.local>
2025-09-20 13:02:14 +0530

    reflection system

--- LangGraph/react_agent_basic.py
+++ LangGraph/react_agent_basic.py
@@ -1,21 +1,26 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-from langchain.agents import initialize_agent
+from langchain.agents import initialize_agent, tool
 from langchain_community.tools import TavilySearchResults
-
+import datetime
 
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
-search_tool = TavilySearchResults(search_depth='basic')
+search_tool = TavilySearchResults(search_depth="basic")
 
-tools = [search_tool]
+@tool
+def get_system_time(format: str = "%Y-%m-%d %H:%M:%S"):
+    """ Returns the current date and time in the specified format """
+
+    current_time = datetime.datetime.now()
+    formatted_time = current_time.strftime(format)
+    return formatted_time
+
+
+tools = [search_tool, get_system_time]
 
 agent = initialize_agent(tools=tools, llm=llm, agent="zero-shot-react-description", verbose=True)
 
-agent.invoke("Give me a funny tweet about todays weather in mumbai")
-
-# result = llm.invoke("Write a poem about a lonely computer")
-
-# print(result)
+agent.invoke("When was SpaceX's last launch and how many days ago was that from this instant")
